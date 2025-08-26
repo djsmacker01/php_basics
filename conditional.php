d42af281159e7bb72eb349923a5114553886c0d2
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-08-26 11:00:24 +0100

    update conditional

--- conditional.php
+++ conditional.php
@@ -60,14 +60,42 @@ echo "....While Loop..."."<br>";
 
  $count = 1;
      while($count < 5){
-        echo "count: $count<br>";
+        echo "count (while): $count<br>";
         $count++;
      }
  echo "<br>";
 // for loop
 echo "....For Loop..."."<br>";
 for($i = 1; $i <= 15; $i++){
-    echo "count: $i<br>";
+    echo "count (for): $i<br>";
 }   
+echo "<br>";
+// for each loop (arrays)
+$Fruits = ["Apple", "Banana", "Cherry"];
+foreach($Fruits as $fruit)
+     {
+        echo "Fruit: $fruit<br>";
+     }
+/*
+Your Exercise
+At the bottom of day3_conditionals.php, add:
+A variable $number = 10.
+Use if...else to check if it’s even or odd.
+Create an array of 3 favorite movies.
+Use foreach to display them one by one.
 
+*/
+
+$number = 10
+
+if($number % 2 == 0){
+    echo "The number is even.";
+} else {
+    echo "The number is odd.";
+}
+
+$movies = ["Inception", "The Matrix", "Interstellar"];
+foreach($movies as $movie) {
+    echo "Favorite Movie: $movie<br>";
+}
 ?>
