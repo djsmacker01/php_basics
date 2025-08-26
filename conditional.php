b2f1bbe8a7c4d85f0de683a8a85a78da749612bd
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-08-26 08:41:06 +0100

    conditional statement

---
+++ conditional.php
@@ -0,0 +1,56 @@
+<?php
+ // conditional and looping statements
+
+ $age = 20;
+
+ if($age > 18){
+    echo "You are an adult.";
+ }
+ else{
+    echo "You are not an adult.";
+ }
+
+ // if..elseif...else
+ $score = 75;
+   if($score >= 90){
+    echo "Grade: A<br>";
+   }
+   elseif($score >= 75){
+    echo "Grade: B<br>";
+   }
+   elseif($score >= 50){
+    echo "Grade: C<br>";
+   }
+   else{
+    echo "Grade: F<br>";
+   }
+
+// switch statement
+
+$day = "Monday";
+
+switch($day){
+    case "Monday":
+        echo "Start of the week!. <br>";
+        break;
+    case "Tuesday":
+        echo "Second day of the week!.<br>";
+        break;
+    case " Wednesday":
+        echo "Midweek day!.<br>";
+        break;
+
+    case "Thursday":
+        echo "Almost the weekend!.<br>";
+        break;
+    case "Friday":
+        echo "End of the work week!.<br>";
+        break;
+
+    case "Saturday":
+        echo "It's the weekend!.<br>";
+        break;
+}
+
+
+?>
